6c4cd12561825a55cfaa01801ed24773973fc992
ArkIiii-9526 <3325506868@qq.com>
2025-06-10 14:25:22 +0800

    解决了MyBatis-plus的依赖问题

--- src/test/java/com/manage/boot/BootApplicationTests.java
+++ src/test/java/com/manage/boot/BootApplicationTests.java
@@ -1,13 +1,28 @@
 package com.manage.boot;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import static org.springframework.test.util.AssertionErrors.*;
 
 @SpringBootTest
 class BootApplicationTests {
+    @Autowired
+    private DataSource dataSource;
 
     @Test
     void contextLoads() {
+        try (Connection connection = dataSource.getConnection()) {
+            assertNotNull("数据库连接不应为null", connection);
+            assertTrue("连接应有效", connection.isValid(5));
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
